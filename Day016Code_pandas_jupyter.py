5c82d7d29cd48b0d966b819ce5f0d47e39f2dd3e
Karl Smith <karl@karlsclipboard.com>
2020-06-27 09:40:19 -0400

    100DaysOfCode 016/100 - working with Pandas and jupyter

---
+++ Day016Code_pandas_jupyter.py
@@ -0,0 +1,21 @@
+#-----------------------------------------------
+#100DaysOfCode 016/100 - working with Pandas
+#   2020-06-27
+#
+#   Pandas is used to analize data
+#     Will need a libraary to work with Pandas
+#        use the following command on the command line:
+#              pip3 install pandas
+#     Also installing IPython - enhanced Python interactive shell
+#              pip3 install ipython
+#     I had to install ipython to be able to access it:
+#          sudo apt install ipython3
+#    Installing jupyter notebook
+#          pip3 install jupyter
+#          sudo snap install jupyter
+#   Jupyter runs from the command line in the current folder with:
+#        jupyter notebook
+#     CTL + ENTER --- executes jupyter code 
+#
+#
+#

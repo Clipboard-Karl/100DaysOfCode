fedf91fbe2fab47eb9b71ac9cc91e8bdd394812b
Karl Smith <karl@karlsclipboard.com>
2020-07-08 07:10:24 -0400

    100DaysOfCode 0xx/100 - working with folium - now usin GitHub for revs

--- Day023Code_map04.py
+++ Day023Code_map04.py
@@ -1,8 +1,11 @@
-#100DaysOfCode 023/100 - I am trying to add to the code and note replace the
-#        the code each day for the larger assignments.  I didn't take into
-#        account that I was not running out of my GitHub library.
-#        Something to learn for tomorrow.
+#100DaysOfCode 0xx/100 - This is the file that contains my python mapping
+#     assignment.  It took some trouble shooting however I am now running from
+#     my local GitHub clone.  So GitHub will be my version control and not
+#     me renaming files and saving.
 #
+#  20200708 - 100DaysOfCode 024/100 - Adding points form a .txt file
+#             The color was not changing because I was using colors not colors
+#             I am concerned that the .txt file would need error checking first
 #  20200707 - Adding points to the map
 #  20200705 - Cleaning the code
 #  20200704 - This is a continuation of day 20.
@@ -45,18 +48,34 @@ map_fg = folium.FeatureGroup("Carlisle Map")
 #  Elements can be saved to the map
 #      The next lines will add a markers
 #---------------------------
-map_fg.add_child(folium.Marker(location=[40.201452,-77.201452], popup="Carlisle is here", icon=folium.Icon(colors='green')))
-map_fg.add_child(folium.Marker(location=[40.2023587,-77.2065121], popup="Leo's", icon=folium.Icon(colors='green')))
+map_fg.add_child(folium.Marker(location=[40.201452,-77.201452], popup="Carlisle is here", icon=folium.Icon(color='green')))
+map_fg.add_child(folium.Marker(location=[40.2023587,-77.2065121], popup="Leo's", icon=folium.Icon(color='green')))
 #---------------------------
 #  markers can be added with a for loop itteration
 for marker_coordinates in [[40.25,-77.25],[40.3,-77.1]]:
-    map_fg.add_child(folium.Marker(location=marker_coordinates, popup="For Loop", icon=folium.Icon(Colors="yellow")))
-
+    map_fg.add_child(folium.Marker(location=marker_coordinates, popup="For Loop", icon=folium.Icon(color="orange")))
+#
+#  The following code will read a .txt file to add markers
+import pandas
+#  create a DataFrame from the file
+volcano_data = pandas.read_csv("Files/Mapping/Volcanoes.txt")
+#  create lists from the DataFrame
+#     working on a list is faster than wroking with a DataFrame
+#   Karl Note:  I don't like this - unless I am missing something this is assuming
+#               the .txt file has no erros or missing numbers.  Maybe more will
+#               be said about this later - or the file was validated ??????
+volcano_lat = list(volcano_data["LAT"])
+volcano_lon = list(volcano_data["LON"])
+#
+#  iterate throught the lists with the zip function
+for lt, ln in zip(volcano_lat, volcano_lon):
+    map_fg.add_child(folium.Marker(location=[lt, ln], popup=".txt Loop", icon=folium.Icon(color="red")))
+#
 map_object.add_child(map_fg)
 #
 #save the map object
 #    Note - don't forget when working in the python shell the default directory
 #      is used.  When executing the python code here a file path is needed
 #      if you want to use a specific file folder
-map_object.save("files/mapping/Home_Map.html")
+map_object.save("Files/Mapping/Home_Map.html")
 #  My browser paath is -- file:///home/karl/python/files/mapping/Home_Map.html
